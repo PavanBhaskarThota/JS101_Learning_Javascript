2a65d0ba621c49d028f297d339b1b92203f25262
PavanBhaskarThota <pavanbhaskarthota05@gmail.com>
2023-04-17 11:53:10 +0530

    variables

---
+++ day1variables.js
@@ -0,0 +1,77 @@
+
+// Day1 Assignment 
+
+// Problem 1
+
+console.log("Masai School");
+console.log("A Transformation in Education");
+
+
+// Problem 2
+
+
+let name = "Thota Pavan Bhaskar";
+  
+console.log("Name          :",name);
+
+name = "Thota Satya Prakash";
+  
+console.log("Father's Name :",name);
+
+name = "Thota Uma Annapurna";
+  
+console.log("Morthe's Name :",name);
+
+
+// Problem 3
+
+let name = "Thota Pavan Bhaskar";
+let age = 23;
+
+console.log(name);
+console.log(typeof(name));
+
+console.log(age);
+console.log(typeof(age);
+
+// Probelm 4
+
+let name = "J Ankith";
+let school = "GreenField School";
+let grade = "A";
+let section = "S-206";
+let rollNum = 45;
+let english = 83;
+let maths = 87;
+let science = 72;
+
+
+console.log("-----------------------------------------------------");
+console.log("");
+console.log("                  *** Report Card ***                ")
+console.log("");
+console.log("  Student Name : ",name);
+console.log("  Section      : ",section);
+console.log("  Roll Number  : ",rollNum);
+console.log("  School       : ",school);
+console.log("");
+console.log("  Subject Names            Marks");
+console.log("")
+console.log("  English                  ",english);
+console.log("  Maths                    ",maths);
+console.log("  Science                  ",science);
+console.log("");
+console.log("  Total Marks              ",english+maths+science);
+console.log("");
+console.log("  Grade                     ",grade);
+console.log("");
+console.log("");
+console.log("");
+console.log("                                   ______________");
+console.log("                                    Guardian Sign");
+console.log("");
+
+
+
+
+
